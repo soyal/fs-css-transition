2f93beeda6ea5a08ef84af5c615c983129d6b8c3
Soyal <345723940@qq.com>
2018-06-09 18:17:43 +0800

    init

---
+++ stories/base.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BaseDemo extends Component {
+  render() {
+    return (
+      <div>base demo</div>
+    )
+  }
+}
+
+export default BaseDemo
---
+++ stories/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import BaseDemo from './base'
+
+storiesOf('示例', module).add('基础示例', () => (
+  <BaseDemo></BaseDemo>
+))
